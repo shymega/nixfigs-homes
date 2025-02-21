7e0d42512de93419a41510e2216803f399b6338e
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:27 +0000

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ homes/default.nix
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{ inputs, ... }:
+let
+  genPkgs =
+    system:
+    import inputs.nixpkgs {
+      inherit system;
+      overlays = builtins.attrValues self.overlays;
+      config = self.nixpkgs-config;
+    };
+  inherit (inputs) self;
+  inherit (inputs.home-manager.lib) homeManagerConfiguration;
+  genConfiguration =
+    _hostname:
+    {
+      type,
+      hostPlatform,
+      username,
+      ...
+    }:
+    let
+      libx = inputs.nixfigs-helpers.libx.${hostPlatform};
+    in
+    homeManagerConfiguration {
+      pkgs = libx.genPkgs hostPlatform;
+      modules = [ ./configs ];
+      extraSpecialArgs = {
+        system = hostPlatform;
+        hostType = type;
+        pkgs = libx.genPkgs hostPlatform;
+        inherit
+          inputs
+          username
+          self
+          libx
+          ;
+      };
+    };
+in
+inputs.nixpkgs.lib.mapAttrs genConfiguration (
+  inputs.nixpkgs.lib.filterAttrs (_: host: host.type == "home-manager") self.hosts
+)
