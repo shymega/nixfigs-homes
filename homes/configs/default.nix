87dd904d9cb710d23570fb009a4100754aafaf68
Dom Rodriguez <shymega@shymega.org.uk>
2025-03-28 17:39:02 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- homes/configs/default.nix
+++ homes/configs/default.nix
@@ -15,6 +15,10 @@
   inherit (libx) isPC homePrefix;
   inherit (lib) getExe getExe';
   isModule = builtins.hasAttr "osConfig" args;
+  osConfig =
+    if isModule
+    then builtins.getAttr "osConfig" args
+    else {};
   getHomeDirectory = username: homePrefix + "/${username}";
   homeDirectory = getHomeDirectory username;
   flatpakPackages = [
@@ -75,15 +79,16 @@ in {
   imports =
     [
       ./network-targets.nix
-      (import ./programs/rofi.nix {inherit lib pkgs;})
+      ./programs/rofi.nix
+      ./programs/hyprland.nix
       inputs.agenix.homeManagerModules.default
-      inputs.nix-doom-emacs-unstraightened.hmModule
       inputs.nix-index-database.hmModules.nix-index
       inputs._1password-shell-plugins.hmModules.default
       inputs.shypkgs-public.hmModules.${system}.dwl
       inputs.nix-flatpak.homeManagerModules.nix-flatpak
       inputs.nixfigs-secrets.user
       inputs.lix-module.nixosModules.default
+      inputs.shyemacs-cfg.homeModules.emacs
     ]
     ++ (
       if !isModule
@@ -177,6 +182,8 @@ in {
         android-tools
         asciinema
         aws-sam-cli
+        awscli2
+        azure-cli
         b4
         bat
         bc
@@ -205,6 +212,7 @@ in {
         fzf
         glab
         gnumake
+        go
         google-chrome
         google-cloud-sdk
         gthumb
@@ -215,12 +223,10 @@ in {
         imapsync
         inetutils
         ispell
-        itd
         jdk17
         jq
         leafnode
         libnotify
-        llm-ls
         m4
         maven
         meli
@@ -233,6 +239,7 @@ in {
         neomutt
         networkmanagerapplet
         nh
+        nixfmt-rfc-style
         nixpacks
         nixpkgs-fmt
         nodejs
@@ -280,13 +287,13 @@ in {
         totp
         units
         unrar
+        unstable.weechatWithMyPlugins
         unzip
         vdirsyncer
         vlc
         w3m
         wayfarer
         wayvnc
-        unstable.weechatWithMyPlugins
         wezterm
         wf-recorder
         wget
@@ -297,9 +304,14 @@ in {
         zellij
         zip
         zoxide
+        (pkgs.doomEmacs {
+          doomDir = inputs.nixfigs-doom-emacs;
+          doomLocalDir = "${homeDirectory}/.local/state/doom";
+          emacs = pkgs.emacs29-pgtk;
+        })
+        (pkgs.isync.override {withCyrusSaslXoauth2 = true;})
       ]
       ++ [inputs.agenix.packages.${system}.default]
-      ++ [(pkgs.isync.override {withCyrusSaslXoauth2 = true;})]
       ++ (
         with pkgs;
           lib.optionals isPC (
@@ -307,10 +319,13 @@ in {
               [
                 clion
                 datagrip
+                dataspell
                 gateway
                 goland
+                idea-community
                 idea-ultimate
                 phpstorm
+                pycharm-community
                 pycharm-professional
                 rider
                 ruby-mine
@@ -334,7 +349,6 @@ in {
                 wineWowPackages.stable
                 winetricks
                 xrlinuxdriver
-                zenmonitor
               ])
           )
       )
@@ -382,11 +396,15 @@ in {
         allow-loopback-pinentry
       '';
     };
-    kanshi = {
-      enable = true;
-      systemdTarget = "wlroots-session.target";
-      settings = import ./aux/kanshi-config.nix;
-    };
+    kanshi =
+      if isModule && builtins.hasAttr "osConfig.networking.hostName" args
+      then
+        lib.optionalAttrs (lib.hasInfix args.osConfig.networking.hostName "-LINUX") {
+          enable = true;
+          systemdTarget = "wlroots-session.target";
+          settings = import ./aux/kanshi-config.nix;
+        }
+      else {};
     gnome-keyring = {
       enable = true;
       components = ["secrets"];
@@ -549,7 +567,7 @@ in {
       lfs.enable = true;
       extraConfig = {
         #        gpg.format = "ssh";
-        #        "gpg \"ssh\"".program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+        #        "gpg \"ssh\"".program = "${getExe' pkgs._1password-gui "op-ssh-sign"}";
         #        commit.gpgsign = true;
       };
       aliases = {
@@ -581,14 +599,6 @@ in {
       nix-direnv.enable = true;
     };
     home-manager.enable = true;
-    doom-emacs = {
-      enable = false;
-      emacs = pkgs.emacs29-pgtk;
-      provideEmacs = true;
-      experimentalFetchTree = true;
-      doomDir = inputs.nixfigs-doom-emacs;
-      doomLocalDir = "${homeDirectory}/.local/state/doom";
-    };
     taskwarrior = {
       enable = true;
       package = pkgs.taskwarrior2;
@@ -681,7 +691,7 @@ in {
           Requires = ["atuin-daemon.socket"];
         };
         Service = {
-          ExecStart = "${lib.getExe' pkgs.unstable.atuin "atuin"} daemon";
+          ExecStart = "${getExe' pkgs.unstable.atuin "atuin"} daemon";
           Environment = ["ATUIN_LOG=info"];
           Restart = "on-failure";
           RestartSteps = 5;
