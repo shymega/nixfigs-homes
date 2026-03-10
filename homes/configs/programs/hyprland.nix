596b9caca225711dba96a8474cdeb091ba86cc93
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-10 16:59:55 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- homes/configs/programs/hyprland.nix
+++ homes/configs/programs/hyprland.nix
@@ -25,6 +25,16 @@ in {
       # hy3.packages.${pkgs.stdenv.hostPlatform.system}.hy3
       split-monitor-workspaces.packages.${pkgs.stdenv.hostPlatform.system}.split-monitor-workspaces
     ];
+    extraConfig = ''
+       	plugin {
+      	split-monitor-workspaces {
+      	    count = 10
+           	keep_focused = 0
+         		enable_notifications = 1
+         		enable_persistent_workspaces = 0
+       		}
+      }
+    '';
     settings = {
       bind = [
         "SUPER, Return, exec, alacritty"
@@ -41,32 +51,34 @@ in {
         "$mainMod, down, movefocus, d"
 
         # Switch workspaces with mainMod + [0-9]
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
+        "$mainMod, 1, split-workspace, 1"
+        "$mainMod, 2, split-workspace, 2"
+        "$mainMod, 3, split-workspace, 3"
+        "$mainMod, 4, split-workspace, 4"
+        "$mainMod, 5, split-workspace, 5"
+        "$mainMod, 6, split-workspace, 6"
+        "$mainMod, 7, split-workspace, 7"
+        "$mainMod, 8, split-workspace, 8"
+        "$mainMod, 9, split-workspace, 9"
+        "$mainMod, 0, split-workspace, 10"
 
         # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
+        "$mainMod SHIFT, 1, split-movetoworkspace, 1"
+        "$mainMod SHIFT, 2, split-movetoworkspace, 2"
+        "$mainMod SHIFT, 3, split-movetoworkspace, 3"
+        "$mainMod SHIFT, 4, split-movetoworkspace, 4"
+        "$mainMod SHIFT, 5, split-movetoworkspace, 5"
+        "$mainMod SHIFT, 6, split-movetoworkspace, 6"
+        "$mainMod SHIFT, 7, split-movetoworkspace, 7"
+        "$mainMod SHIFT, 8, split-movetoworkspace, 8"
+        "$mainMod SHIFT, 9, split-movetoworkspace, 9"
+        "$mainMod SHIFT, 0, split-movetoworkspace, 10"
+
+        "$mainMod ALT, M, split-cycleworkspaces, next"
 
         # Move workspace to monitor
-        "$mainMod ALT, left, movecurrentworkspacetomonitor, l"
-        "$mainMod ALT, right, movecurrentworkspacetomonitor, r"
+        "$mainMod ALT, left, split-changemonitor, prev"
+        "$mainMod ALT, right, split-changemonitor, next"
 
         # full screen
         "SUPER, F, fullscreen"
@@ -274,7 +286,7 @@ in {
       };
       listener = [
         {
-          timeoout = 300;
+          timeout = 300;
           on-timeout = lib.getExe lock_cmd;
         }
         {
