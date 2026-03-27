7fe26792ef69265f7d71a7724d18b77682cc0191
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-27 18:47:41 +0000

    chore: Bump to Hyprland v0.54.3
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -127,7 +127,7 @@
       };
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.54.2";
+      url = "github:hyprwm/Hyprland?ref=v0.54.3";
     };
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins?ref=v0.53.0";
