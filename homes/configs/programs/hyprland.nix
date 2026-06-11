be31892bed321ff217e0d16fe6ce545976d41ed4
Dom Rodriguez <shymega@shymega.org.uk>
2026-06-11 20:08:43 +0100

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- homes/configs/programs/hyprland.nix
+++ homes/configs/programs/hyprland.nix
@@ -286,20 +286,20 @@ in {
     enable = true;
     settings = {
       general = rec {
-	lock_cmd = "pidof hyprlock || hyprlock";
+        lock_cmd = "pidof hyprlock || hyprlock";
         before_sleep_cmd = lock_cmd;
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
       listener = [
-      	{
-	  timeout = 300;
+        {
+          timeout = 300;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
-	}
-	{
-	  timeout = 302;
-	  on-timeout = "loginctl lock-session";
-	}
+        }
+        {
+          timeout = 302;
+          on-timeout = "loginctl lock-session";
+        }
       ];
     };
   };
